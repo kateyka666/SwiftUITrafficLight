ec99717323ac81e4e2cbe3c510dcae49b326b2e1
Ios Kateyka <ioskateyka@gmail.com>
2021-07-26 22:38:23 +0300

    Initial Commit

---
+++ SwiftUITrafficLight/SwiftUITrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITrafficLightApp.swift
+//  SwiftUITrafficLight
+//
+//  Created by Екатерина Боровкова on 26.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
