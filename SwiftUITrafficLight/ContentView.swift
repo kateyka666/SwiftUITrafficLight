303b0761c9b73672989b6d9c7ca40a51ce9dfacc
Ios Kateyka <ioskateyka@gmail.com>
2021-07-29 22:26:32 +0300

    wellDone

--- SwiftUITrafficLight/ContentView.swift
+++ SwiftUITrafficLight/ContentView.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     
-   @State private var currentColor: ColorSwitchingLogic = .defaultState
+    @State private var currentColor: ColorSwitchingLogic = .defaultState
     
     @State private var red =  CircleView(color: .red,alpha: alphaIsOff)
     @State private var yellow =  CircleView(color: .yellow, alpha: alphaIsOff)
     @State private var green = CircleView(color: .green, alpha: alphaIsOff)
     
+   
     var body: some View {
         ZStack {
             
@@ -22,19 +23,22 @@ struct ContentView: View {
             let linearGradient = LinearGradient(gradient: colors, startPoint: .leading, endPoint: .trailing)
             
             Color.black.ignoresSafeArea()
+            
             VStack( spacing: 20) {
                 
                 red
                 yellow
                 green
+                
                 Spacer()
+                
                 Button(action: {
                     choiseColorForLightning()
                 }) {
                     Text(
                         currentColor != .defaultState ? "NEXT" : "Start"
                     )
-                        .font(.largeTitle).bold()
+                    .font(.largeTitle).bold()
                 }
                 .frame(width: 200, height: 100)
                 .background(linearGradient)
@@ -56,22 +60,22 @@ struct ContentView_Previews: PreviewProvider {
 
 extension ContentView {
     private func choiseColorForLightning(){
-          switch currentColor {
-          case .defaultState:
-              red = CircleView(color: .red,alpha: alphaIsOn)
-              currentColor = .red
-          case .red:
-              red = CircleView(color: .red,alpha: alphaIsOff)
-             yellow =  CircleView(color: .yellow, alpha: alphaIsOn)
-              currentColor = .orange
-          case .orange:
-              yellow = CircleView(color: .yellow, alpha: alphaIsOff)
-              green = CircleView(color: .green, alpha: alphaIsOn)
-              currentColor = .green
-          case .green:
-              red = CircleView(color: .red,alpha: alphaIsOn)
-              green = CircleView(color: .green, alpha: alphaIsOff)
-              currentColor = .red
-          }
+        switch currentColor {
+        case .defaultState:
+            red = CircleView(color: .red,alpha: alphaIsOn)
+            currentColor = .red
+        case .red:
+            red = CircleView(color: .red,alpha: alphaIsOff)
+            yellow =  CircleView(color: .yellow, alpha: alphaIsOn)
+            currentColor = .orange
+        case .orange:
+            yellow = CircleView(color: .yellow, alpha: alphaIsOff)
+            green = CircleView(color: .green, alpha: alphaIsOn)
+            currentColor = .green
+        case .green:
+            red = CircleView(color: .red,alpha: alphaIsOn)
+            green = CircleView(color: .green, alpha: alphaIsOff)
+            currentColor = .red
+        }
     }
 }
